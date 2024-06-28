70cf78d66d51c181981696e1880a0d9534018e14
Adam Wanninger <ajwann@ajwann.com>
2024-06-27 22:07:55 -0400

    initial API call working, next step full convo

---
+++ index.js
@@ -0,0 +1,54 @@
+const {VertexAI} = require('@google-cloud/vertexai');
+
+// Initialize Vertex with your Cloud project and location
+const vertex_ai = new VertexAI({project: 'sincere-loader-427823-m3', location: 'us-central1'});
+const model = 'gemini-1.5-flash-001';
+
+// Instantiate the models
+const generativeModel = vertex_ai.preview.getGenerativeModel({
+  model: model,
+  generationConfig: {
+    'maxOutputTokens': 8192,
+    'stopSequences': ['cheese'],
+    'temperature': 1,
+    'topP': 0.95,
+  },
+  safetySettings: [
+    {
+        'category': 'HARM_CATEGORY_HATE_SPEECH',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_HARASSMENT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    }
+  ],
+});
+
+
+async function generateContent() {
+  const req = {
+    contents: [
+      {role: 'user', parts: [{text: `You are hypebot. Your goal is to hype up individuals and make them feel great about themselves.`}]}
+    ],
+  };
+
+  const streamingResp = await generativeModel.generateContentStream(req);
+
+  for await (const item of streamingResp.stream) {
+    process.stdout.write('stream chunk: ' + JSON.stringify(item) + '\n');
+  }
+
+  process.stdout.write('aggregated response: ' + JSON.stringify(await streamingResp.response));
+}
+
+generateContent();
+
